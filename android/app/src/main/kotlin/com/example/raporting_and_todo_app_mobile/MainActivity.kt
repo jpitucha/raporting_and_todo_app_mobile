0f0238dbbf1017e3c4cbd3c4863a7bfc9b4d89ae
Jakub Pitucha <vaguevans@gmail.com>
2019-12-09 22:32:04 +0100

    Project added

---
+++ android/app/src/main/kotlin/com/example/raporting_and_todo_app_mobile/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.raporting_and_todo_app_mobile
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
