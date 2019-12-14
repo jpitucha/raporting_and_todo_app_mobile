87317800b135503bb22b8549cf3a8631440cfba0
Jakub Pitucha <vaguevans@gmail.com>
2019-12-14 03:12:35 +0100

    Added removing items from shippings list

--- lib/pages/shipping.dart
+++ lib/pages/shipping.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../widgets/tile.dart';
 import '../pack.dart';
 
+enum ConfirmAction { CANCEL, ACCEPT }
+
 class ShippingPage extends StatefulWidget {
   ShippingPageState createState() => ShippingPageState();
 }
@@ -16,6 +18,34 @@ class ShippingPageState extends State<ShippingPage> {
     super.initState();
   }
 
+  Future<ConfirmAction> _deleteConfirmDialog(BuildContext context) async {
+  return showDialog<ConfirmAction>(
+    context: context,
+    barrierDismissible: false, // user must tap button for close dialog!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Czy aby na pewno?'),
+        content: const Text(
+            'To spowoduje usunięcie elementu na zawsze'),
+        actions: <Widget>[
+          FlatButton(
+            child: const Text('ZAMKNIJ'),
+            onPressed: () {
+              Navigator.of(context).pop(ConfirmAction.CANCEL);
+            },
+          ),
+          FlatButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop(ConfirmAction.ACCEPT);
+            },
+          )
+        ],
+      );
+    },
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +55,18 @@ class ShippingPageState extends State<ShippingPage> {
       body: ListView.builder(
         itemCount: packages.length,
         itemBuilder: (BuildContext context, int index) {
-          return Tile(packages[index].title, packages[index].status, () {}, () {setState(() { packages.removeAt(index); }); });
+          return Tile(packages[index].title, packages[index].status,
+          () {
+            //edit
+          },
+          () async {
+            final ConfirmAction choice = await _deleteConfirmDialog(context);
+            if (choice == ConfirmAction.ACCEPT) {
+              setState(() {
+                packages.removeAt(index);
+              });
+            }
+          });
         }
       ),
       floatingActionButton: FloatingActionButton(
